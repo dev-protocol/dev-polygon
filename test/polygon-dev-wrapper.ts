43a3bec719596dfb4c437865713e5d416db25950
aggre <hiroyuki.aggre@gmail.com>
2022-01-08 00:59:35 +0900

    initial implementation

--- test/polygon-dev-wrapper.ts
+++ test/polygon-dev-wrapper.ts
@@ -1,28 +1,12 @@
 import { expect } from 'chai'
-import IIboxABI from '../abi/interfaces/IInbox.sol/IInbox.json'
-import IOutboxABI from '../abi/interfaces/IOutbox.sol/IOutbox.json'
-import IBridgeABI from '../abi/interfaces/IBridge.sol/IBridge.json'
-import { ethers, upgrades, waffle } from 'hardhat'
-
-const { deployMockContract } = waffle
+import { ethers, upgrades } from 'hardhat'
 
 describe('PolygonDevWrapper', () => {
-	/** TODO - these need to be changed */
-	const L2_TOKEN_ADDR = '0x0000000000000000000000000000000000000000'
-	const ROUTER_ADDR = '0x0000000000000000000000000000000000000001'
-	const GATEWAY_ADDR = '0x0000000000000000000000000000000000000002'
 	const DUMMY_MINT_AMOUNT = 10 ** 8
-	/** End TODO */
 
 	before(async function () {
 		this.Dev = await ethers.getContractFactory('PolygonDevWrapper')
 		this.ErcDummy = await ethers.getContractFactory('DummyDev')
-
-		const [, user] = await ethers.getSigners()
-
-		this.mockBridge = await deployMockContract(user, IBridgeABI)
-		this.mockInbox = await deployMockContract(user, IIboxABI)
-		this.mockOutbox = await deployMockContract(user, IOutboxABI)
 	})
 
 	beforeEach(async function () {
@@ -34,120 +18,97 @@ describe('PolygonDevWrapper', () => {
 
 		await this.ercDummy.deployed()
 
-		this.dev = await upgrades.deployProxy(
-			this.Dev,
-			[
-				L2_TOKEN_ADDR,
-				ROUTER_ADDR,
-				GATEWAY_ADDR,
-				this.mockInbox.address,
-				this.ercDummy.address,
-			],
-			{ unsafeAllow: ['delegatecall'] }
-		)
+		this.dev = await upgrades.deployProxy(this.Dev, [this.ercDummy.address], {
+			unsafeAllow: ['delegatecall'],
+			initializer: '__init',
+		})
 		await this.dev.deployed()
+		await this.ercDummy.grantRole(
+			'0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6',
+			this.dev.address
+		)
 	})
 
 	it('Should initialize values', async function () {
-		expect(await this.dev.l2Token()).to.equal(L2_TOKEN_ADDR)
-		expect(await this.dev.gateway()).to.equal(GATEWAY_ADDR)
-		expect(await this.dev.inbox()).to.equal(this.mockInbox.address)
+		expect(await this.dev.name()).to.equal('Polygon Dev Wrapper')
+		expect(await this.dev.symbol()).to.equal('WDEV')
 		expect(await this.dev.devAddress()).to.equal(this.ercDummy.address)
 	})
 
-	describe('escrowMint', () => {
-		it('Should fail minting when not from l2Token address', async function () {
-			await this.mockBridge.mock.activeOutbox.returns(this.mockOutbox.address)
+	describe('Wrap', () => {
+		it('Should successfully wrap DEV', async function () {
+			const [user] = await ethers.getSigners()
+			const wrapAmount = 100
 
-			await this.mockOutbox.mock.l2ToL1Sender.returns(GATEWAY_ADDR) // What should this testing be?
-
-			await this.mockInbox.mock.bridge.returns(this.mockBridge.address)
-
-			expect(this.dev.escrowMint(100)).to.be.revertedWith(
-				'sender must be l2 token'
+			expect(await this.dev.balanceOf(user.address)).to.eq(0)
+			expect(await this.ercDummy.balanceOf(user.address)).to.eq(
+				DUMMY_MINT_AMOUNT
 			)
+			expect(await this.ercDummy.balanceOf(this.dev.address)).to.eq(0)
+
+			await this.ercDummy.approve(this.dev.address, wrapAmount)
+			await this.dev.wrap(wrapAmount)
+
+			expect(await this.dev.balanceOf(user.address)).to.eq(wrapAmount)
+			expect(await this.ercDummy.balanceOf(user.address)).to.eq(
+				DUMMY_MINT_AMOUNT - wrapAmount
+			)
+			expect(await this.ercDummy.balanceOf(this.dev.address)).to.eq(wrapAmount)
 		})
-		it('Should successfully minting WDEV and DEV when the caller is lsToken', async function () {
-			const [, l2Token, router, gateway] = await ethers.getSigners()
-			// Create a WDEV contract that initializes the dummy l2Token as an L2 Token
-			const wdev = await upgrades.deployProxy(
-				this.Dev,
-				[
-					l2Token.address,
-					router.address,
-					gateway.address,
-					this.mockInbox.address,
-					this.ercDummy.address,
-				],
-				{ unsafeAllow: ['delegatecall'] }
+		it('Should fail wrapping due to insufficient DEV balance', async function () {
+			const [, user2] = await ethers.getSigners()
+			this.ercDummy.connect(user2).approve(this.dev.address, 100)
+
+			expect(this.dev.connect(user2).wrap(100)).to.be.revertedWith(
+				'Insufficient balance'
 			)
-			await wdev.deployed()
-			const wdevBalanceOfGateway1 = await wdev.balanceOf(gateway.address)
-			const devBalanceOfWdev1 = await this.ercDummy.balanceOf(wdev.address)
-
-			// Mocking
-			await this.mockInbox.mock.bridge.returns(this.mockBridge.address)
-			await this.mockBridge.mock.activeOutbox.returns(this.mockOutbox.address)
-			await this.mockOutbox.mock.l2ToL1Sender.returns(l2Token.address)
-			await this.ercDummy.grantRole(
-				ethers.utils.id('MINTER_ROLE'),
-				wdev.address
-			)
-
-			await wdev.connect(l2Token).escrowMint(100).catch(console.log)
-
-			const wdevBalanceOfGateway2 = await wdev.balanceOf(gateway.address)
-			const devBalanceOfWdev2 = await this.ercDummy.balanceOf(wdev.address)
-
-			expect(wdevBalanceOfGateway2.sub(100)).to.be.equal(wdevBalanceOfGateway1)
-			expect(devBalanceOfWdev2.sub(100)).to.be.equal(devBalanceOfWdev1)
 		})
 	})
 
-	it('Should fail wrapping due to insufficient DEV balance', async function () {
-		const [, user2] = await ethers.getSigners()
-		this.ercDummy.connect(user2).approve(this.dev.address, 100)
+	describe('Unwrap', () => {
+		it('Should successfully unwrap DEV', async function () {
+			const [user] = await ethers.getSigners()
+			const wrapAmount = 100
 
-		expect(this.dev.connect(user2).wrap(100)).to.be.revertedWith(
-			'Insufficient balance'
-		)
-	})
+			const wrapping = async () => {
+				await this.ercDummy.approve(this.dev.address, wrapAmount)
+				await this.dev.wrap(wrapAmount)
+				expect(await this.dev.balanceOf(user.address)).to.eq(wrapAmount)
+			}
 
-	it('Should successfully wrap DEV', async function () {
-		const [user] = await ethers.getSigners()
-		const wrapAmount = 100
+			await wrapping()
 
-		expect(await this.dev.balanceOf(user.address)).to.eq(0)
-		expect(await this.ercDummy.balanceOf(user.address)).to.eq(DUMMY_MINT_AMOUNT)
-		expect(await this.ercDummy.balanceOf(this.dev.address)).to.eq(0)
+			await this.dev.unwrap(wrapAmount)
 
-		await this.ercDummy.approve(this.dev.address, wrapAmount)
-		await this.dev.wrap(wrapAmount)
+			expect(await this.dev.balanceOf(user.address)).to.eq(0)
+			expect(await this.ercDummy.balanceOf(user.address)).to.eq(
+				DUMMY_MINT_AMOUNT
+			)
+			expect(await this.ercDummy.balanceOf(this.dev.address)).to.eq(0)
+		})
+		it('Should successfully mint DEV when unwrapping DEV', async function () {
+			const [, user1] = await ethers.getSigners()
+			const wrapAmount = 100
 
-		expect(await this.dev.balanceOf(user.address)).to.eq(wrapAmount)
-		expect(await this.ercDummy.balanceOf(user.address)).to.eq(
-			DUMMY_MINT_AMOUNT - wrapAmount
-		)
-		expect(await this.ercDummy.balanceOf(this.dev.address)).to.eq(wrapAmount)
-	})
+			const prepare = async () => {
+				await this.dev.mint(user1.address, wrapAmount)
+				expect(await this.dev.balanceOf(user1.address)).to.eq(wrapAmount)
+				expect(await this.ercDummy.balanceOf(this.dev.address)).to.eq(0)
+			}
 
-	it('Should fail unwrapping due to insufficient pegged DEV funds', async function () {
-		expect(this.dev.unwrap(1000)).to.be.revertedWith('Insufficient balance')
-	})
+			await prepare()
 
-	it('Should successfully unwrap DEV', async function () {
-		const [user] = await ethers.getSigners()
-		const wrapAmount = 100
+			await this.dev.connect(user1).unwrap(wrapAmount)
 
-		await this.ercDummy.approve(this.dev.address, wrapAmount)
-		await this.dev.wrap(wrapAmount)
-		expect(await this.dev.balanceOf(user.address)).to.eq(wrapAmount)
-
-		await this.dev.unwrap(wrapAmount)
-
-		expect(await this.dev.balanceOf(user.address)).to.eq(0)
-		expect(await this.ercDummy.balanceOf(user.address)).to.eq(DUMMY_MINT_AMOUNT)
-		expect(await this.ercDummy.balanceOf(this.dev.address)).to.eq(0)
+			expect(await this.dev.balanceOf(user1.address)).to.eq(0)
+			expect(await this.ercDummy.balanceOf(user1.address)).to.eq(wrapAmount)
+			expect(await this.ercDummy.totalSupply()).to.eq(
+				wrapAmount + DUMMY_MINT_AMOUNT
+			)
+		})
+		it('Should fail unwrapping due to insufficient pegged DEV funds', async function () {
+			expect(this.dev.unwrap(1000)).to.be.revertedWith('Insufficient balance')
+		})
 	})
 
 	describe('transferDev', () => {
